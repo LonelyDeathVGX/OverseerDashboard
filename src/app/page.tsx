dbfc43425455dfc1642bd68310bb0b3725f7ba28
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-08 18:43:11 +0200

    Little changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,6 @@ export default function Page() {
   return (
     <main>
       <NavbarComponent isDashboard={false} />
-      <div className="px-12 py-24">
-        <div className="flex items-center justify-center">
-          <h1 className="font-bold text-xl text-white">Hola</h1>
-        </div>
-      </div>
     </main>
   );
 }
---
+++ src/components/ui/Card.tsx
@@ -0,0 +1,36 @@
+import { cn } from "@/lib/Util";
+import * as React from "react";
+
+const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("rounded-lg bg-default-900 text-white", className)} {...props} />
+));
+Card.displayName = "Card";
+
+const CardHeader = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => (
+    <div ref={ref} className={cn("flex flex-col space-y-1.5 p-6", className)} {...props} />
+  ),
+);
+CardHeader.displayName = "CardHeader";
+
+const CardTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+  ({ className, ...props }, ref) => <h3 ref={ref} className={cn("font-bold text-2xl", className)} {...props} />,
+);
+CardTitle.displayName = "CardTitle";
+
+const CardDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
+  ({ className, ...props }, ref) => <p ref={ref} className={cn("text-default-400 text-sm", className)} {...props} />,
+);
+CardDescription.displayName = "CardDescription";
+
+const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => <div ref={ref} className={cn(className)} {...props} />,
+);
+CardContent.displayName = "CardContent";
+
+const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props }, ref) => <div ref={ref} className={cn("flex items-center", className)} {...props} />,
+);
+CardFooter.displayName = "CardFooter";
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
