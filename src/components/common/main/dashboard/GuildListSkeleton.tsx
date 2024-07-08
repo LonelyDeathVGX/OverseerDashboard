83355fc9459a190f7649864e29b8291a8a6cb015
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-08 18:21:06 +0200

    Update `GuildListSkeleton`

--- src/components/common/main/dashboard/GuildListSkeleton.tsx
+++ src/components/common/main/dashboard/GuildListSkeleton.tsx
@@ -1,17 +1,22 @@
-import { Card, CardBody, CardFooter } from "@nextui-org/react";
-import { Skeleton } from "@nextui-org/skeleton";
+import { Card, CardContent } from "@/components/ui/Card";
+import { Skeleton } from "@/components/ui/Skeleton";
 
 export function GuildListSkeletonComponent() {
   return Array(4)
     .fill(undefined)
     .map((index) => (
-      <Card key={index} isPressable={true}>
-        <CardBody className="p-0 overflow-hidden">
-          <Skeleton className="w-full object-cover h-[140px] rounded-b-none" />
-        </CardBody>
-        <CardFooter>
-          <p className="font-medium text-center text-small w-full">Loading...</p>
-        </CardFooter>
+      <Card key={index} className="overflow-hidden">
+        <CardContent className="relative">
+          <Skeleton className="h-48 w-full object-cover" />
+          <div className="absolute top-0 flex h-full w-full flex-col items-center justify-center gap-4 p-4">
+            <div className="size-24 rounded-full bg-default-900/50 p-2 backdrop-blur-sm">
+              <Skeleton className="size-full rounded-full" />
+            </div>
+            <div className="w-full rounded-lg bg-default-900/50 p-2 text-center font-medium text-xs backdrop-blur-sm">
+              Loading...
+            </div>
+          </div>
+        </CardContent>
       </Card>
     ));
 }
