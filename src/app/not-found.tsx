a367db090d0087fe988c29a05de7ba740bb8ea33
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-08 17:52:47 +0200

    Update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,23 @@
+import { Unlink2 } from "@/components/Icons";
 import { NavbarComponent } from "@/components/common/main/navbar/Navbar";
+import { Button } from "@/components/ui/Button";
+import Link from "next/link";
 
 export default function Page() {
   return (
     <main>
       <NavbarComponent isDashboard={false} />
-      <div className="px-12 py-24">
-        <div className="flex flex-col gap-4">
-          <h1 className="font-bold text-7xl text-white text-center">404</h1>
-          <p className="font-medium text-sm text-foreground-500 text-center">
-            If you think this is an error
-            <br />
-            please contact us at our support server
-          </p>
+      <div className="full-page flex items-center justify-center py-12">
+        <div className=" flex w-full max-w-[1024px] flex-col items-center justify-center gap-6 px-7">
+          <div className="rounded-lg border border-default-800 p-6">
+            <Unlink2 className="size-36 text-white" />
+          </div>
+          <h1 className="text-center font-bold text-white text-xl">This page could not be found</h1>
+          <Button asChild={true} variant="outline">
+            <Link target="_self" href="/">
+              Go to main page
+            </Link>
+          </Button>
         </div>
       </div>
     </main>
