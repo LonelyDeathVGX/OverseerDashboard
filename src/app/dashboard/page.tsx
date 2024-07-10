c6015bda7119e0f1179625a6d3a7cdb61f757ac8
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-10 15:54:50 +0200

    Add `terms` and `privacy` pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <NavbarComponent isDashboard={false} />
       <div className="flex items-center justify-center py-12">
         <div className="flex w-full max-w-5xl flex-col gap-6 px-5">
-          <h1 className="font-bold text-3xl text-white">Manage a server</h1>
+          <h1 className="font-bold text-3xl text-white">Manage a Server</h1>
           <div className="grid grid-cols-1 xs:grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4">
             <Suspense fallback={<GuildListSkeletonComponent />}>
               <GuildListComponent />
---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,22 @@
+import { NavbarComponent } from "@/components/common/main/navbar/Navbar";
+import { CircleAlert } from "lucide-react";
+import { Alert, AlertDescription } from "#ui/Alert";
+
+export default function Page() {
+  return (
+    <main>
+      <NavbarComponent isDashboard={false} />
+      <div className="flex items-center justify-center py-12">
+        <div className="flex w-full max-w-5xl flex-col gap-6 px-5">
+          <h1 className="font-bold text-3xl text-white">Privacy Policy</h1>
+          <div className="flex flex-col gap-6">
+            <Alert variant="warning">
+              <CircleAlert className="size-5" />
+              <AlertDescription>This page is under construction.</AlertDescription>
+            </Alert>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
