d0ed703a92bad5cb5f62497a20e2060cc3a2313d
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-12 15:57:39 +0200

    Dashboard components

--- src/app/dashboard/[id]/layout.tsx
+++ src/app/dashboard/[id]/layout.tsx
@@ -1,11 +1,8 @@
-import { SidebarComponent } from "@/components/common/dashboard/sidebar/Sidebar";
-import { NavbarComponent } from "@/components/common/main/navbar/Navbar";
-import { ADD_TO_DISCORD_WITH_GUILD_ID_URL } from "@/lib/Constants";
-import { fetchClientGuild } from "@/lib/Requests";
-import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
+import { NavbarComponent } from "#components/navbar/Navbar";
+import { SidebarComponent } from "./components/Sidebar";
 
-export default async function Layout({
+export default function Layout({
   children,
   params,
 }: Readonly<{
@@ -14,21 +11,11 @@ export default async function Layout({
     id: string;
   };
 }>) {
-  const { found, error } = await fetchClientGuild(params.id);
-
-  if (error) {
-    return redirect(`/?error=${encodeURIComponent("Unable to obtain the server")}`);
-  }
-
-  if (!found) {
-    return redirect(ADD_TO_DISCORD_WITH_GUILD_ID_URL(params.id));
-  }
-
   return (
     <div>
       <NavbarComponent isDashboard={true} />
       <div>
-        <SidebarComponent />
+        <SidebarComponent guildID={params.id} />
         <main className="p-8 md:ml-80">{children}</main>
       </div>
     </div>
--- src/app/dashboard/[id]/page.tsx
+++ src/app/dashboard/[id]/page.tsx
@@ -1,56 +1,33 @@
-"use client";
+import { Info } from "lucide-react";
+import { fetchClientGuild } from "#lib/Requests";
+import { Alert, AlertDescription, AlertTitle } from "#ui/Alert";
+import { Card, CardHeader, CardTitle } from "#ui/Card";
 
-import { useGuild } from "@/lib/contexts/Guild";
-import { Avatar, Card, CardBody, CardFooter, CardHeader, Divider, Select, SelectItem } from "@nextui-org/react";
-import { type ChangeEvent, useState } from "react";
-
-const Languages: {
-  key: string;
-  label: string;
-}[] = [
-  {
-    key: "en",
-    label: "English",
-  },
-  {
-    key: "es",
-    label: "Spanish",
-  },
-];
-
-export default function Page() {
-  const { guild } = useGuild();
-  const [language, setLanguage] = useState<string>("en");
-
-  const handleLanguageChange = (event: ChangeEvent<HTMLSelectElement>) => {
-    setLanguage(event.target.value);
+export default async function Page({
+  params,
+}: Readonly<{
+  params: {
+    id: string;
   };
+}>) {
+  const { guild } = await fetchClientGuild(params.id);
 
   return (
-    <main>
-      <div className="py-12 flex items-center justify-center">
-        <div className="max-w-[1024px] w-full px-6 flex flex-col gap-6">
-          <Card>
-            <CardHeader className="p-6 font-bold font-white text-xl">General Configuration</CardHeader>
-            <Divider />
-            <CardBody className="p-6">
-              <Select
-                label="Language"
-                labelPlacement="outside"
-                placeholder="Select a language"
-                className="font-medium"
-                onChange={handleLanguageChange}
-              >
-                {Languages.map((language) => (
-                  <SelectItem key={language.key} value={language.key} className="font-medium">
-                    {language.label}
-                  </SelectItem>
-                ))}
-              </Select>
-            </CardBody>
-          </Card>
+    <div className="flex flex-col gap-6">
+      <Alert>
+        <Info className="size-5" />
+        <div>
+          <AlertTitle>Welcome to the Overseer Dashboard</AlertTitle>
+          <AlertDescription>
+            This Dashboard is still under development, so we welcome feedback from all our users.
+          </AlertDescription>
         </div>
-      </div>
-    </main>
+      </Alert>
+      <Card>
+        <CardHeader>
+          <CardTitle>{guild?.name}</CardTitle>
+        </CardHeader>
+      </Card>
+    </div>
   );
 }
