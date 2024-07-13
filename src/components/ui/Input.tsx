00c73a9421a391071c38f0532d44815c542ce8f0
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-13 21:30:53 +0200

    Add `Input`

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { cn } from "../Util";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        "flex h-10 w-full rounded-lg bg-default-900 p-2 font-medium text-sm text-white placeholder:text-default-400 disabled:cursor-not-allowed disabled:opacity-50",
+        className,
+      )}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = "Input";
+
+export { Input };
