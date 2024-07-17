6ceec09e990f0d9d5a428e557d37d426fa2454b1
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-17 13:45:09 +0200

    Add `robots` and `sitemap`

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+import { BASE_URL } from "#lib/Constants";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/api/",
+    },
+    sitemap: `${BASE_URL}/sitemap.xml`,
+  };
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import type { MetadataRoute } from "next";
+import { BASE_URL } from "#lib/Constants";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: BASE_URL,
+      changeFrequency: "weekly",
+    },
+    {
+      url: `${BASE_URL}/privacy`,
+      changeFrequency: "weekly",
+    },
+    {
+      url: `${BASE_URL}/terms`,
+      changeFrequency: "weekly",
+    },
+  ];
+}
