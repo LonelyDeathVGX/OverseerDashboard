5251a256a813f52d8a768d8e9a1042f6ce7aaf9c
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-23 20:41:34 +0200

    Added `husky`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
       <NavbarComponent isDashboard={false} />
       <div className="flex items-center justify-center py-8">
         <div className="flex w-full max-w-5xl flex-col gap-6 px-8">
-          <div className="flex flex-col gap-6"></div>
+          <div className="flex flex-col gap-6" />
         </div>
       </div>
     </main>
