6a86f3d4f85e2b43ffa115d2a5f97b2269d8947d
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-07-31 20:35:42 +0200

    Did some little changes

--- src/components/AdSense.tsx
+++ src/components/AdSense.tsx
@@ -1,6 +1,3 @@
-// import Script from "next/script";
-
 export function AdSenseComponent() {
-  return <meta name="google-adsense-account" content="ca-pub-8874019743704741" />;
-  // return <Script async={true} src={process.env.AD_SENSE_URL} crossOrigin="anonymous" />;
+  return <meta content="ca-pub-8874019743704741" name="google-adsense-account" />;
 }
