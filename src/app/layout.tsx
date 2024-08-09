b4be69fa6d36bb8ff8c80df48b1c321645e60896
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-09 20:47:59 +0200

    Use `viewport` and `metadata` and replaced functions to arrow functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,37 +7,33 @@ import type { ReactNode } from "react";
 import { AdSenseComponent } from "#components/AdSense";
 import { DevToolsComponent } from "#components/DevTools";
 import { BASE_URL } from "#lib/Constants";
-import { metadata } from "#metadata";
+import { createMetadata } from "#metadata";
 import { Toaster } from "#ui/Toaster";
 
-export function generateMetadata(): Metadata {
-  return metadata({
-    canonical: BASE_URL,
-    description:
-      "Overseer is a Discord bot created with the purpose of being robust and manage Discord Servers in a better and easy way.",
-    robots: {
+export const metadata: Metadata = createMetadata({
+  canonical: BASE_URL,
+  description:
+    "Overseer is a Discord bot created with the purpose of being robust and manage Discord Servers in a better and easy way.",
+  robots: {
+    follow: true,
+    googleBot: {
       follow: true,
-      googleBot: {
-        follow: true,
-        index: true,
-      },
       index: true,
     },
-    title: "Overseer - A better way to manage Discord Servers",
-  });
-}
-export function generateViewport(): Viewport {
-  return {
-    themeColor: "#2b2d31",
-  };
-}
-export const runtime = "edge";
+    index: true,
+  },
+  title: "Overseer - A better way to manage Discord Servers",
+});
+export const viewport: Viewport = {
+  themeColor: "#2b2d31",
+};
+export const runtime: "nodejs" | "edge" = "edge";
 
-export default function RootLayout({
+export default ({
   children,
 }: {
   children: ReactNode;
-}) {
+}) => {
   return (
     <html className="bg-black font-medium font-poppins text-white leading-relaxed tracking-wide antialiased" lang="en">
       <head>
@@ -50,4 +46,4 @@ export default function RootLayout({
       </body>
     </html>
   );
-}
+};
