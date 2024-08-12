c45cace0c6285a934ae02701b8486d4318e8d045
LonelyDeath-VGX <lonelydeathvgx@gmail.com>
2024-08-12 12:53:58 +0200

    Use one object

--- src/app/dashboard/[guildID]/general/configuration/page.tsx
+++ src/app/dashboard/[guildID]/general/configuration/page.tsx
@@ -1,27 +1,22 @@
 import { GuildConfigurationManager } from "#lib/database/managers/GuildConfiguration";
-import { GeneralConfigurationComponent } from "../_lib/components/GeneralConfiguration";
-import { PremiumComponent } from "../_lib/components/Premium";
+import { GeneralConfigurationComponent } from "../_lib/components/configuration/GeneralConfiguration";
+import { PremiumComponent } from "../_lib/components/configuration/Premium";
 
-export default async function Page({
+export default async ({
   params,
 }: {
   params: {
     guildID: string;
   };
-}) {
+}) => {
   const guildConfiguration = await GuildConfigurationManager.findOne({
     guildID: params.guildID,
   });
 
   return (
     <div className="flex flex-col gap-6">
-      <GeneralConfigurationComponent
-        data={{
-          locale: guildConfiguration?.general.locale.toLowerCase() ?? "en",
-        }}
-        guildID={params.guildID}
-      />
+      <GeneralConfigurationComponent data={guildConfiguration?.general ?? {}} guildID={params.guildID} />
       <PremiumComponent data={guildConfiguration?.premium ?? {}} guildID={params.guildID} />
     </div>
   );
-}
+};
